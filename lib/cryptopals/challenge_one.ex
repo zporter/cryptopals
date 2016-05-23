bbabdf69d3166f2845e400d3124a43ba471247d2
Zachary Porter <zporter9@gmail.com>
2016-05-23 12:32:26 -0400

    Challenge 2: Fixed XOR

--- lib/cryptopals/challenge_one.ex
+++ lib/cryptopals/challenge_one.ex
@@ -1,6 +1,25 @@
 defmodule Cryptopals.ChallengeOne do
+  use Bitwise, only_operators: true
+
   def hex_to_base64(hex_string) do
-    decoded_string = Base.decode16!(hex_string, case: :lower)
-    Base.encode64(decoded_string)
+    hex_decode(hex_string)
+    |> Base.encode64
   end
+
+  def xor_combine(input1, input2) do
+    binary_list1 = :binary.bin_to_list(hex_decode(input1))
+    binary_list2 = :binary.bin_to_list(hex_decode(input2))
+
+    Stream.zip(binary_list1, binary_list2)
+    |> Stream.map(fn({a, b}) -> a ^^^ b end)
+    |> Enum.to_list
+    |> IO.chardata_to_string
+    |> Base.encode16(case: :lower)
+  end
+
+
+  defp hex_decode(input) do
+    Base.decode16!(input, case: :lower)
+  end
+
 end
--- test/cryptopals_test.exs
+++ test/cryptopals_test.exs
@@ -8,4 +8,12 @@ defmodule CryptopalsTest do
 
     assert Cryptopals.ChallengeOne.hex_to_base64(hex) == base64
   end
+
+  test "XOR combination" do
+    input1   = "1c0111001f010100061a024b53535009181c"
+    input2   = "686974207468652062756c6c277320657965"
+    expected = "746865206b696420646f6e277420706c6179"
+
+    assert Cryptopals.ChallengeOne.xor_combine(input1, input2) == expected
+  end
 end
